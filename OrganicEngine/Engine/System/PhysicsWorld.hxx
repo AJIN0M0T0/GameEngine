137dce03b18057852b8ae017ba8747ac8106ae32
yuki57-46 <yuki.rno@gmail.com>
2025-03-30 19:33:02 +0900

    共通物理演算クラスの定義とインクルードの追加

--- OrganicEngine/Engine/System/PhysicsWorld.hxx
+++ OrganicEngine/Engine/System/PhysicsWorld.hxx
@@ -1,16 +1,28 @@
 /**
  * @file PhysicsWorld.hxx
- * @brief 
+ * @brief 共通物理演算クラス
  * @author yuki57-46
  * @date 2025/03/29 
  */
 
+
+// =-=-= インクルードガード部 =-=-=
 #ifndef ____PHYSICS_WORLD_HXX____
 #define ____PHYSICS_WORLD_HXX____
 
+// =-=-= インクルード部 =-=-=
+#include <btBulletDynamicsCommon.h>
+#include <btBulletCollisionCommon.h>
+
+#include "Singleton.hxx"
 
 
-
+namespace Engine::System {
+	class PhysicsWorld : public Engine::System::Singleton<PhysicsWorld>
+	{
+		friend class Singleton<PhysicsWorld>;
+	}
+}
 
 
 
